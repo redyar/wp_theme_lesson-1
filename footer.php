67c87f2798dff7c3e90d313c09ceafd49e789598
IgorEf <winterisred@gmail.com>
2024-02-15 15:05:23 +0400

    perenos

---
+++ footer.php
@@ -0,0 +1,21 @@
+      <!-- Footer Type-1 -->
+      <footer class="footer footer-type-1 bg-dark">
+        <div class="bottom-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 copyright text-center">
+                <span>
+                  © <?php echo date('Y') ?> <?php bloginfo('name') ?> - <?php bloginfo('description') ?>
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </footer> <!-- end footer -->
+
+    </div> <!-- end content wrapper -->
+  </div> <!-- end main wrapper -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title><?php echo wp_get_document_title() ?></title>
+
+  <meta charset="<?php bloginfo('charset') ?>">
+  <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="description" content="">
+
+  <!-- Google Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:300,400%7COpen+Sans:400,400i,700%7CMerriweather:400ii?subset=cyrillic' rel='stylesheet'>
+
+  
+<?php wp_head(); ?>
+</head>
+
+<body class="relative">
+
+  <!-- Preloader -->
+  <div class="loader-mask">
+    <div class="loader">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+
+  <div class="main-wrapper oh">
+
+    <header class="nav-type-1 dark-nav">
+      <!-- Fullscreen search -->
+      <div class="search-wrap">
+        <div class="search-inner">
+          <div class="search-cell">
+            <form method="get">
+              <div class="search-field-holder">
+                <input type="search" class="form-control main-search-input" placeholder="Найти что-то...">
+                <div class="search-submit-icon"><i class="icon icon_search"></i></div>
+                <input type="submit" class="search-submit" value="search">
+              </div>
+            </form>
+          </div>
+        </div>
+        <i class="icon icon_close search-close" id="search-close"></i>
+      </div> <!-- end fullscreen search -->
+
+      <nav class="navbar navbar-fixed-top">
+        <div class="navigation">
+          <div class="container relative">
+
+            <div class="row">
+
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                  <span class="sr-only">Навигация</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+              </div> <!-- end navbar-header -->
+
+
+              <!-- side menu -->
+              <div class="side-menu nav-left hidden-sm hidden-xs">
+								<div class="nav-inner">
+                    <a href="#" class="logo">Курс Миши</a>
+                </div>
+              </div> <!-- end side menu -->
+
+              <div class="col-md-12 nav-wrap">
+                <div class="collapse navbar-collapse text-center" id="navbar-collapse">
+
+                  <!-- <ul class="nav navbar-nav">
+
+                    <li class="active">
+                      <a href="index.html">Главная</a>
+                    </li>
+
+                    <li class="dropdown">
+                      <a href="index.html">Блог</a>
+                      <ul class="dropdown-menu">
+                        <li><a href="category.html">Лайфстайл</a></li>
+                        <li><a href="category.html">Трэвел</a></li>
+                        <li><a href="category.html">Бьюти</a></li>
+                        <li><a href="category.html">Ворк хард</a></li>
+												<li><a href="category.html">Еда</a></li>
+                      </ul>
+                    </li>
+
+                    <li>
+                      <a href="about.html">Обо мне</a>
+                    </li>
+
+                    <li>
+                      <a href="contact.html">Контакты</a>
+                    </li>
+
+                    <li id="mobile-search" class="hidden-lg hidden-md">
+                      <form method="get" class="mobile-search">
+                        <input type="search" class="form-control" placeholder="Search...">
+                        <button type="submit" class="search-button">
+                          <i class="icon icon_search"></i>
+                        </button>
+                      </form>
+                    </li>
+
+                  </ul> end menu -->
+
+                  <?php
+                  if(has_nav_menu('header')){
+                    $mobile_search = '      <li id="mobile-search" class="hidden-lg hidden-md">
+                                              <form method="get" class="mobile-search">
+                                                <input type="search" class="form-control" placeholder="Search...">
+                                                <button type="submit" class="search-button">
+                                                  <i class="icon icon_search"></i>
+                                                </button>
+                                              </form>
+                                            </li>';
+                      wp_nav_menu(array(
+                        'theme_location' => 'header',
+                        'container'      => false,
+                        'menu_class'     => 'nav navbar-nav',
+                        'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s' . $mobile_search . '</ul>',
+                        'depth'          => 2,
+                        'walker'         => new Lesson_Nav_Menu(), 
+                      
+                      ));
+                    }
+                  ?>
+
+                </div> <!-- end collapse -->
+              </div> <!-- end col -->
+
+              <!-- side menu -->
+              <div class="side-menu right mobile-left-align">
+								<div class="nav-inner">
+                  <div class="nav-search-wrap hidden-sm hidden-xs right">
+                    <a href="#" class="nav-search search-trigger">
+                      <i class="icon icon_search"></i>
+                    </a>
+                  </div>
+                </div>
+              </div> <!-- end side menu -->
+
+            </div> <!-- end row -->
+          </div> <!-- end container -->
+        </div> <!-- end navigation -->
+      </nav> <!-- end navbar -->
+    </header>
