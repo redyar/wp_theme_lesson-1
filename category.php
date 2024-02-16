9b14589d90782633bb352801524991465ae6a9e0
IgorEf <winterisred@gmail.com>
2024-02-16 22:00:36 +0400

    get_template_part

--- category.php
+++ category.php
@@ -18,38 +18,7 @@
               <?php if( have_posts() ) : ?>
                 <div class="row items-grid">
                   <?php while( have_posts() ) : the_post(); ?>
-                    <div class="col-sm-6">
-                      <article class="entry-item">
-                        <div class="entry-img">
-                          <a href="blog-single.html">
-                            <img src="img/post_1.jpg" alt="">
-                          </a>
-                        </div>
-                        <div class="entry-header">
-                        <div  class="entry-category"><?php the_category(',  '); ?></div>
-                          <h2 class="entry-title">
-                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                          </h2>
-                        </div>
-                        <div class="entry-wrap">
-                          <div class="entry">
-                            <div class="entry-content">
-                              <p><?php the_excerpt(); ?></p>
-                            </div>
-                            <div class="entry-meta-wrap clearfix">
-                              <ul class="entry-meta">
-                                <li class="entry-date">
-                                  <a href="#"><?php the_time('j M Y') ?></a>
-                                </li>
-                                <li class="entry-comments">
-                                  <a href="blog-single.html">5 комментариев</a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </article>
-                    </div>
+                    <?php get_template_part('template_parts/post', 'content') ?>
                   <?php endwhile; ?>
                 </div> 
               <?php else: ?>
