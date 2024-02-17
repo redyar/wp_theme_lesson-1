a9ef4a8a02e6448800e4ed652eec5d192a0b7375
IgorEf <winterisred@gmail.com>
2024-02-17 16:53:33 +0400

    posts

--- index.php
+++ index.php
@@ -11,17 +11,26 @@
               <!-- large post -->
               <article class="entry-item large-post">
                 <?php
-                $i = 0;
+                // $i = 0;
  
                 while( have_posts() ) : the_post();
-                  $i++;
-                  if( 1 === $i ) {
-                    // если первая запись, то подключаем файл entry-featured.php
-                    get_template_part( 'template_parts/main', 'post' );
-                  } else {
-                    // иначе подключаем обычный entry.php
-                    get_template_part( 'template_parts/post', 'content' );
-                  }
+                  
+                if(is_sticky()){
+                  get_template_part( 'template_parts/main', 'post' );
+                }else{
+                  get_template_part( 'template_parts/post', 'content' );
+                }
+
+
+
+                //   $i++;
+                //   if( !is_paged() && 1 === $i ) {
+                //     // если первая запись, то подключаем файл entry-featured.php
+                //     get_template_part( 'template_parts/main', 'post' );
+                //   } else {
+                //     // иначе подключаем обычный entry.php
+                //     get_template_part( 'template_parts/post', 'content' );
+                //   }
                  
                 endwhile;
                 
@@ -36,15 +45,6 @@
                   'next_text' => '<i class="icon arrow_carrot-right"></i>'
                   
                   ) );?>
-                  <!-- <a href="#" class=""><i class="icon arrow_carrot-left"></i></a>
-									<a href="#" class="">2</a>
-									<a href="#" class="">3</a>
-									<a href="#" class="">4</a>
-									<span class="current">5</span>
-									<span>...</span>
-									<a href="#" class="">4</a>
-									<a href="#" class=""><i class="icon arrow_carrot-right"></i></a> -->
-
                 </div>
               </div>
 
