f1ac64c80770274750dd3b41605a45fec994d715
IgorEf <winterisred@gmail.com>
2024-02-17 16:16:14 +0400

    pagination

--- index.php
+++ index.php
@@ -24,19 +24,21 @@
                   }
                  
                 endwhile;
+                
                 ?>
               </article> <!-- end large post -->
-
+              
               <div class="row mt-20">
                 <div class="col-md-12 text-center pagination">
-                  <a href="#" class=""><i class="icon arrow_carrot-left"></i></a>
+                <?php echo paginate_links( array('prev_next' => false) );?>
+                  <!-- <a href="#" class=""><i class="icon arrow_carrot-left"></i></a>
 									<a href="#" class="">2</a>
 									<a href="#" class="">3</a>
 									<a href="#" class="">4</a>
 									<span class="current">5</span>
 									<span>...</span>
 									<a href="#" class="">4</a>
-									<a href="#" class=""><i class="icon arrow_carrot-right"></i></a>
+									<a href="#" class=""><i class="icon arrow_carrot-right"></i></a> -->
 
                 </div>
               </div>
