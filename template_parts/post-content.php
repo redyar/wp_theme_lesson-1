e4c3b7770a785caaf7dd691ca7c740d6e554ff90
IgorEf <winterisred@gmail.com>
2024-02-17 19:08:45 +0400

    img

--- template_parts/post-content.php
+++ template_parts/post-content.php
@@ -1,32 +1,32 @@
 <div class="col-sm-6">
-                      <article class="entry-item">
-                        <div class="entry-img">
-                          <a href="blog-single.html">
-                            <img src="img/post_1.jpg" alt="">
-                          </a>
-                        </div>
-                        <div class="entry-header">
-                        <div  class="entry-category"><?php the_category(',  '); ?></div>
-                          <h2 class="entry-title">
-                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                          </h2>
-                        </div>
-                        <div class="entry-wrap">
-                          <div class="entry">
-                            <div class="entry-content">
-                              <p><?php the_excerpt(); ?></p>
-                            </div>
-                            <div class="entry-meta-wrap clearfix">
-                              <ul class="entry-meta">
-                                <li class="entry-date">
-                                  <a href="#"><?php the_time('j M Y') ?></a>
-                                </li>
-                                <li class="entry-comments">
-                                  <a href="blog-single.html">5 комментариев</a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </article>
-                    </div>
+  <article class="entry-item">
+  <div class="entry-img">
+			<a href="<?php the_permalink() ?>">
+				<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/post_1.jpg" alt="">
+			</a>
+		</div>
+    <div class="entry-header">
+    <div  class="entry-category"><?php the_category(',  '); ?></div>
+      <h2 class="entry-title">
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      </h2>
+    </div>
+    <div class="entry-wrap">
+      <div class="entry">
+        <div class="entry-content">
+          <p><?php the_excerpt(); ?></p>
+        </div>
+        <div class="entry-meta-wrap clearfix">
+          <ul class="entry-meta">
+            <li class="entry-date">
+              <a href="#"><?php the_time('j M Y') ?></a>
+            </li>
+            <li class="entry-comments">
+              <a href="blog-single.html">5 комментариев</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </article>
+</div>
